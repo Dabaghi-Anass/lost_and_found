03bbf32e11cfc9151ed378c61b72f77505ee80ff
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2024-12-27 22:57:02 +0100

    added image view element

--- app/(app)/declare-item/[option].tsx
+++ app/(app)/declare-item/[option].tsx
@@ -11,7 +11,7 @@ import * as Location from 'expo-location';
 import { useLocalSearchParams } from "expo-router";
 import { ChevronLeft, ChevronRight } from 'lucide-react-native';
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, TouchableNativeFeedback, View } from 'react-native';
 type OptionType = "lost" | "found";
 interface FormData {
   type: OptionType;
@@ -20,10 +20,40 @@ interface FormData {
   description: string;
   color: string;
   location: string;
+  images: string[];
   coordinates: { latitude: number; longitude: number } | null;
   date: Date;
 }
 
+function ImagesPreview({ images, onSelect }: { images: string[], onSelect?: (index: number) => void }) {
+  return (<View className='flex flex-col gap-2'>
+    <Image
+      source={{ uri: images[0] }}
+      style={{
+        width: "100%",
+        aspectRatio: 16 / 9,
+        borderRadius: 8,
+      }}
+    />
+    <View className="flex-row gap-2 flex-wrap">
+      {images.slice(1).map((image, i) => (
+        <TouchableNativeFeedback onPress={() => onSelect?.(i)}>
+          <Image
+            className='border-2 border-white shadow-lg'
+            source={{ uri: image }}
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 8,
+            }}
+            key={i}
+          />
+        </TouchableNativeFeedback>
+      ))}
+    </View>
+  </View>
+  );
+}
 
 export default function DeclareItemScreen() {
   const { option }: { option: OptionType } = useLocalSearchParams();
@@ -35,6 +65,14 @@ export default function DeclareItemScreen() {
     description: '',
     color: '#6366f1',
     location: '',
+    images: [
+      "https://firebasestorage.googleapis.com/v0/b/anass-dabaghi-portfolio.appspot.com/o/projects%20images%2Fperspective.jpg?alt=media&token=514f2aa2-4181-4edb-87cd-457632648762",
+      "https://firebasestorage.googleapis.com/v0/b/anass-dabaghi-portfolio.appspot.com/o/projects%20images%2FCapture2.PNG?alt=media&token=c08aa4c4-e299-4575-ab99-1e2fc40d6fe9",
+      "https://firebasestorage.googleapis.com/v0/b/anass-dabaghi-portfolio.appspot.com/o/projects%20images%2FWhatsApp%20Image%202023-07-13%20at%2012.15.32.jpeg?alt=media&token=ff66097e-882f-4555-ad99-4fd2851abc03",
+      "https://firebasestorage.googleapis.com/v0/b/anass-dabaghi-portfolio.appspot.com/o/projects%20images%2Faimate.PNG?alt=media&token=12a8a933-1fcb-442d-aa07-c125e29fd8b6",
+      "https://firebasestorage.googleapis.com/v0/b/anass-dabaghi-portfolio.appspot.com/o/projects%20images%2FWhatsApp%20Image%202023-07-13%20at%2013.44.03.jpeg?alt=media&token=94b24f4d-8741-4a6d-b39c-a9ae0c0af033",
+      "https://firebasestorage.googleapis.com/v0/b/anass-dabaghi-portfolio.appspot.com/o/projects%20images%2FScreenshot%20from%202024-08-07%2015-40-18.png?alt=media&token=c9581925-b004-423f-a017-5b25236adbe9",
+    ],
     coordinates: null,
     date: new Date(),
   });
@@ -45,7 +83,7 @@ export default function DeclareItemScreen() {
     setFormData((prev) => ({ ...prev, [field]: value }));
   };
 
-  const nextStep = () => setStep((prev) => Math.min(prev + 1, 3));
+  const nextStep = () => setStep((prev) => Math.min(prev + 1, 4));
   const prevStep = () => setStep((prev) => Math.max(prev - 1, 1));
 
   const getCurrentLocation = async () => {
@@ -73,11 +111,11 @@ export default function DeclareItemScreen() {
 
   return (
     <ScrollScreen className="flex-1 bg-muted px-4 py-6">
-      <View className="rounded-xl shadow-md flex-1 overflow-hidden h-full bg-red-400">
+      <View className="rounded-xl flex-1 max-h-[80vh] overflow-hidden">
         <LinearGradient
-          className='h-full'
+          className='h-full max-h-[70vh]'
           colors={['#233dfc', '#1e98fc']}
-          start={[0.5, 1]}
+          start={[1, 0.9]}
         >
           <View className="p-4 py-6 h-20">
             <Text className="text-white text-3xl font-bold">
@@ -85,9 +123,9 @@ export default function DeclareItemScreen() {
             </Text>
           </View>
 
-          <View className="p-4 bg-background rounded-t-2xl h-full">
+          <View className="p-4 bg-background rounded-t-2xl h-full flex justify-between">
             <View className="flex-row justify-between mb-6">
-              {['Type', 'Details', 'Location'].map((label, i) => (
+              {['Type', 'Details', 'Location', 'Images'].map((label, i) => (
                 <View key={label} className="items-center">
                   <View
                     className={`h-8 w-8 rounded-full ${i + 1 <= step ? 'bg-indigo-600' : 'bg-gray-300'
@@ -106,7 +144,7 @@ export default function DeclareItemScreen() {
 
             {step === 1 && (
               <View className="py-8">
-                <Text className="font-bold mb-2 text-foreground text-xl">
+                <Text className="font-bold mb-8 text-muted-foreground text-2xl">
                   What would you like to report?
                 </Text>
 
@@ -120,7 +158,7 @@ export default function DeclareItemScreen() {
                           updateFormData('type', 'found');
                         }
                       }} />
-                    <Text className="font-bold text-xl text-foreground">Lost Item</Text>
+                    <Text className="font-bold text-2xl text-foreground">Lost Item</Text>
                   </View>
                   <View className='flex-row gap-4 items-center'>
                     <Checkbox
@@ -131,7 +169,7 @@ export default function DeclareItemScreen() {
                           updateFormData('type', 'lost');
                         }
                       }} />
-                    <Text className="font-bold text-xl text-foreground">Found Item</Text>
+                    <Text className="font-bold text-2xl text-foreground">Found Item</Text>
                   </View>
                 </View>
               </View>
@@ -259,6 +297,23 @@ export default function DeclareItemScreen() {
                 </View>
               </View>
             )}
+            {step === 4 && (
+              <View className="image-upload-form space-y-4 h-[60%] flex flex-start gap-4">
+                <ImagesPreview images={formData.images} onSelect={(index) => {
+                  const images = [...formData.images];
+                  //swap the selected image with the first image
+                  [images[0], images[index + 1]] = [images[index + 1], images[0]];
+                  updateFormData('images', images);
+                }} />
+                <AppButton
+                  onPress={() => console.log('Upload image')}
+                  variant="primary"
+                  className="px-4 py-2 rounded-md">
+                  Upload Image
+                </AppButton>
+
+              </View>
+            )}
             {/*card buttons*/}
             <View className="flex-row justify-between p-4 border-t border-muted mt-8">
               {step > 1 && (
@@ -271,7 +326,7 @@ export default function DeclareItemScreen() {
                   <Text className="ml-2 text-muted-foreground">Back</Text>
                 </AppButton>
               )}
-              {step < 3 ? (
+              {step < 4 ? (
                 <AppButton
                   onPress={nextStep}
                   className="flex-row items-center bg-indigo-600 px-4 py-2 rounded-md ml-auto"
