20f2f5e7e01c8ea9d28d5187fa4fd732cd48a090
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2025-01-19 21:07:15 +0100

    allowed refresh profile

--- app/profile/[id].tsx
+++ app/profile/[id].tsx
@@ -4,22 +4,21 @@ import bgPattern from "@/assets/images/pattern.png";
 import { AppButton } from '@/components/AppButton';
 import { ConfirmationModal } from '@/components/confirmation-modal';
 import ItemMinifiedCard from '@/components/item-minified-card';
-import ScrollScreen from '@/components/scroll-screen';
 import { Badge } from '@/components/ui/badge';
 import { LoadingSpinner } from '@/components/ui/loading-spinner';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { FirebaseCollections } from '@/lib/constants';
 import { setCurrentUser } from '@/redux/global/current-user';
 import { setCurrentScreenName } from '@/redux/global/currentScreenName';
-import { saveUser } from '@/redux/global/users';
+import { saveUser, saveUser as saveUserAction } from '@/redux/global/users';
 import { AppUser, Item } from '@/types/entities.types';
 import { AntDesign, Feather, FontAwesome5 } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Link, router, useLocalSearchParams } from 'expo-router';
 import { Share2 } from 'lucide-react-native';
 import React, { useEffect, useState } from 'react';
 import { Alert, FlatList, Image, Linking, Share, Text, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-
 export default function UserProfile() {
   const { id } = useLocalSearchParams();
   const dispatch = useDispatch();
@@ -75,41 +74,76 @@ export default function UserProfile() {
       })
     setUserItems(items);
   }
-  useEffect(() => {
-    (async () => {
-      setLoading(true);
-      setUserItems([]);
-      setUser(null);
-      if (id) {
-        try {
-          if (usersMap[id as string]) {
-            setUser(usersMap[id as string] as AppUser);
-            getUserItems(usersMap[id as string] as AppUser);
-            return
-          }
-          const userFromDb = await fetchDoc<AppUser>(FirebaseCollections.USERS, id as string, [
+  async function initUser(refresh: boolean = false) {
+    setLoading(true);
+    setUserItems([]);
+    setUser(null);
+    if (id) {
+      try {
+        let userFromDb
+        if (refresh) {
+          userFromDb = await fetchDoc<AppUser>(FirebaseCollections.USERS, id as string, [
             {
               idPropertyName: "profileId",
               propertyName: "profile",
               collectionName: FirebaseCollections.PROFILES
             }
           ]);
-          if (userFromDb) {
-            setUser(userFromDb as AppUser);
-            dispatch(saveUser(userFromDb as AppUser));
-            getUserItems(userFromDb as AppUser)
+          dispatch(saveUserAction(userFromDb as AppUser));
+        } else {
+          if (usersMap[id as string]) {
+            userFromDb = usersMap[id as string];
+            setUser(usersMap[id as string] as AppUser);
+            getUserItems(usersMap[id as string] as AppUser);
+            return
+          } else {
+            userFromDb = await fetchDoc<AppUser>(FirebaseCollections.USERS, id as string, [
+              {
+                idPropertyName: "profileId",
+                propertyName: "profile",
+                collectionName: FirebaseCollections.PROFILES
+              }
+            ]);
+            dispatch(saveUserAction(userFromDb as AppUser));
           }
-        } catch (e: any) {
-          Alert.alert('Error', e.message);
-        } finally {
-          setLoading(false);
         }
-      } else if (!id) {
-        getUserItems(currentUser as AppUser);
-        setUser(currentUser);
+        if (userFromDb) {
+          setUser(userFromDb as AppUser);
+          dispatch(saveUser(userFromDb as AppUser));
+          getUserItems(userFromDb as AppUser)
+        }
+      } catch (e: any) {
+        Alert.alert('Error', e.message);
+      } finally {
+        setLoading(false);
       }
-      setLoading(false);
-    })()
+    } else if (!id) {
+      if (refresh) {
+        const currentUserId = await AsyncStorage.getItem('userID');
+        const currentUser = await fetchDoc<AppUser>(FirebaseCollections.USERS, currentUserId as string, [
+          {
+            idPropertyName: "profileId",
+            propertyName: "profile",
+            collectionName: FirebaseCollections.PROFILES
+          }
+        ]);
+        dispatch(setCurrentUser(currentUser));
+        if (currentUser) {
+          setUser(currentUser);
+          dispatch(saveUser(currentUser));
+          getUserItems(currentUser as AppUser);
+        }
+      } else {
+        if (currentUser) {
+          getUserItems(currentUser as AppUser);
+          setUser(currentUser);
+        }
+      }
+    }
+    setLoading(false);
+  }
+  useEffect(() => {
+    initUser();
   }, [id]);
 
   useEffect(() => {
@@ -117,109 +151,117 @@ export default function UserProfile() {
   }, [user])
   if (!user || !currentUser || loading) return <LoadingSpinner visible={true} />
   return (
-    <ScrollScreen className='w-full h-full'>
-      <View className='bg-transparent flex items-center justify-center py-4 px-4 relative' >
-        <Image source={bgPattern} className='absolute top-0 left-0 right-0 mx-auto' />
-        <Image
-          source={{ uri: user?.profile?.imageUri || 'https://via.placeholder.com/150' }}
-          className='w-32 h-32 rounded-full border-4 border-white'
-        />
-      </View>
-      <View className='bg-background min-h-full rounded-t-3xl p-4'>
-        <View className='flex-row w-min items-center justify-between'>
-          <Text className='text-foreground text-4xl font-bold font-secondary capitalize max-w-sm web:w-[300px] text-center'>{user?.profile?.firstName} {user?.profile?.lastName}</Text>
-          <View className='p-2 gap-4 flex-row items-center justify-center'>
-            <Badge variant="secondary">
-              <Text className='text-secondary-foreground capitalize'>{user.role}</Text>
-            </Badge>
-            <Badge variant="default">
-              <Text className='text-primary-foreground'>{user.items?.length} items</Text>
-            </Badge>
-          </View>
+    <FlatList
+      refreshing={loading}
+      onRefresh={async () => {
+        await initUser(true);
+      }}
+      keyExtractor={item => item.id || Math.random().toString()} data={[user]} renderItem={({ item }) => (<View className='w-full h-full'>
+        <View className='bg-transparent flex items-center justify-center py-4 px-4 relative' >
+          <Image source={bgPattern} className='absolute top-0 left-0 right-0 mx-auto' />
+          <Image
+            source={{ uri: user?.profile?.imageUri || 'https://via.placeholder.com/150' }}
+            className='w-32 h-32 rounded-full border-4 border-white'
+          />
         </View>
-        <View className='flex-row items-center justify-center gap-4 mt-4'>
-          <AppButton size="sm" onPress={handleEmail}>
-            <Feather name="mail" size={20} color="#111" />
-            <Text className='text-lg'>Email</Text>
-          </AppButton>
-        </View>
-        {currentUser.id === user.id &&
-          <View className='flex flex-row items-center justify-center py-8 px-4 gap-4'>
-            <Link href="/edit-profile" asChild>
-              <AppButton variant="outline" className='p-4 gap-4'>
-                <Text className='text-foreground text-xl'>edit profile</Text>
-                <FontAwesome5 name="user-edit" size={20} color={theme === 'dark' ? "white" : "#222"} />
-              </AppButton>
-            </Link>
-            <ConfirmationModal
-              title='Logout'
-              description='Are you sure you want to logout?'
-              onOpen={openConfirmationModal}
-              trigger={(open) => <AppButton variant="destructive" className='gap-4' onPress={() => open?.()}>
-                <Text className='text-white text-xl'>logout</Text>
-                <AntDesign name="logout" size={20} color="white" />
-              </AppButton>}
-              open={confirmationModalVisible}
-              onAccept={() => {
-                handleLogout();
-              }}
-              onClose={() => {
-                setConfirmationModalVisible(false);
-              }}
-            />
-          </View>
-        }
-        <View className='items-start justify-center gap-8 m-5'>
-          <Text className="text-foreground text-2xl font-semibold">Contact Information</Text>
-          <View className='items-start justify-center gap-4'>
-            <View className='flex-row items-center justify-center gap-4'>
-              <Feather name="phone" size={20} color={theme === "dark" ? "white" : "black"} />
-              <Text className="text-foreground text-xl">{user?.profile?.phoneNumber}</Text>
-            </View>
-            <View className='flex-row items-center justify-center gap-4'>
-              <Feather name="mail" size={20} color={theme === "dark" ? "white" : "black"} />
-              <Text className="text-foreground text-xl">{user.email}</Text>
+        <View className='bg-background min-h-full rounded-t-3xl p-4'>
+          <View className='flex-row w-min items-center justify-between'>
+            <Text className='text-foreground text-4xl font-bold font-secondary capitalize max-w-sm web:w-[300px] text-center'>{user?.profile?.firstName} {user?.profile?.lastName}</Text>
+            <View className='p-2 gap-4 flex-row items-center justify-center'>
+              <Badge variant="secondary">
+                <Text className='text-secondary-foreground capitalize'>{user.role}</Text>
+              </Badge>
+              <Badge variant="default">
+                <Text className='text-primary-foreground'>{user.items?.length} items</Text>
+              </Badge>
             </View>
           </View>
-        </View>
-        <View className='flex-row items-center gap-4 p-4'>
-          <AppButton
-            onPress={() => {
-              Linking.openURL(`tel:${user?.profile?.phoneNumber}`)
-            }}
-          >
-            <Feather name="phone-call" size={20} color="#333" />
-            <Text className='text-accent-foreground text-xl font-bold'>Call {user?.profile?.firstName}</Text>
-          </AppButton>
-          <AppButton variant="primary" onPress={handleShareProfile}>
-            <Share2 size={20} color="white" />
-            <Text className='text-white text-xl font-bold'>Share Profile</Text>
-          </AppButton>
-        </View>
-        {userItems.length > 0 &&
-          <View className='items-center justify-center gap-2'>
-            <Text className='text-3xl font-bold text-foreground'>Items ({userItems.length})</Text>
-            <FlatList
-              horizontal
-              scrollEnabled
-              keyExtractor={item => item.id || Math.random().toString()}
-              data={userItems.slice(0, 3)}
-              renderItem={({ item }) => (
-                <ItemMinifiedCard
-                  item={item}
-                />
-              )}
-              contentContainerClassName='gap-4 py-4'
-            />
-            <Link href={`/`} asChild>
-              <AppButton variant="link" size="sm">
-                <Text className='text-primary text-lg'>View all items</Text>
-              </AppButton>
-            </Link>
+          <View className='flex-row items-center justify-center gap-4 mt-4'>
+            <AppButton size="sm" onPress={handleEmail}>
+              <Feather name="mail" size={20} color="#111" />
+              <Text className='text-lg'>Email</Text>
+            </AppButton>
           </View>
-        }
+          {currentUser.id === user.id &&
+            <View className='flex flex-row items-center justify-center py-8 px-4 gap-4'>
+              <Link href="/edit-profile" asChild>
+                <AppButton variant="outline" className='p-4 gap-4'>
+                  <Text className='text-foreground text-xl'>edit profile</Text>
+                  <FontAwesome5 name="user-edit" size={20} color={theme === 'dark' ? "white" : "#222"} />
+                </AppButton>
+              </Link>
+              <ConfirmationModal
+                title='Logout'
+                description='Are you sure you want to logout?'
+                onOpen={openConfirmationModal}
+                trigger={(open) => <AppButton variant="destructive" className='gap-4' onPress={() => open?.()}>
+                  <Text className='text-white text-xl'>logout</Text>
+                  <AntDesign name="logout" size={20} color="white" />
+                </AppButton>}
+                open={confirmationModalVisible}
+                onAccept={() => {
+                  handleLogout();
+                }}
+                onClose={() => {
+                  setConfirmationModalVisible(false);
+                }}
+              />
+            </View>
+          }
+          <View className='items-start justify-center gap-8 m-5'>
+            <Text className="text-foreground text-2xl font-semibold">Contact Information</Text>
+            <View className='items-start justify-center gap-4'>
+              <View className='flex-row items-center justify-center gap-4'>
+                <Feather name="phone" size={20} color={theme === "dark" ? "white" : "black"} />
+                <Text className="text-foreground text-xl">{user?.profile?.phoneNumber}</Text>
+              </View>
+              <View className='flex-row items-center justify-center gap-4'>
+                <Feather name="mail" size={20} color={theme === "dark" ? "white" : "black"} />
+                <Text className="text-foreground text-xl">{user.email}</Text>
+              </View>
+            </View>
+          </View>
+          <View className='flex-row items-center gap-4 p-4'>
+            <AppButton
+              onPress={() => {
+                Linking.openURL(`tel:${user?.profile?.phoneNumber}`)
+              }}
+            >
+              <Feather name="phone-call" size={20} color="#333" />
+              <Text className='text-accent-foreground text-xl font-bold'>Call {user?.profile?.firstName}</Text>
+            </AppButton>
+            <AppButton variant="primary" onPress={handleShareProfile}>
+              <Share2 size={20} color="white" />
+              <Text className='text-white text-xl font-bold'>Share Profile</Text>
+            </AppButton>
+          </View>
+          {userItems.length > 0 &&
+            <View className='items-center justify-center gap-2 w-full'>
+              <Text className='text-3xl font-bold text-foreground'>Items ({userItems.length})</Text>
+              <FlatList
+                horizontal
+                scrollEnabled
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={item => item.id || Math.random().toString()}
+                data={userItems.slice(0, 3)}
+                renderItem={({ item }) => (
+                  <ItemMinifiedCard
+                    item={item}
+                  />
+                )}
+                contentContainerClassName='gap-4 py-4'
+              />
+              <Link href={`/`} asChild>
+                <AppButton variant="link" size="sm">
+                  <Text className='text-primary text-lg'>View all items</Text>
+                </AppButton>
+              </Link>
+            </View>
+          }
 
+        </View>
       </View>
-    </ScrollScreen>
+      )} />
+
   );
 }
